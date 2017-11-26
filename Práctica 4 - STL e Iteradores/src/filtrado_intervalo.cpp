a89115f839a4ee367a67f220a9254375255d8b0e
vicase <vcastro.ugr@gmail.com>
2017-11-26 22:21:42 +0100

    Mains terminados

--- Práctica 4 - STL e Iteradores/src/filtrado_intervalo.cpp
+++ Práctica 4 - STL e Iteradores/src/filtrado_intervalo.cpp
@@ -1,36 +1,43 @@
 #include "cronologia.h"
 #include <fstream>
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
 int main(int argc, char * argv[]){
 
-  if (argc < 4){
+  if (argc < 4 || argc > 5){
       cout<<"Número de argumentos inválido, proporcionar fichero, límite inferior, límite superior y fichero para guardar la cronología(opcional)."<<endl;
       return 0;
    }
 
 
+     ifstream f (argv[1]);
+     if (!f){
+      cout<<"No ha sido posible abrir el fichero "<<argv[1]<<endl;
+      return 0;
+     }
 
-   ifstream f (argv[1]);
-   if (!f){
-    cout<<"No ha sido posible abrir el fichero "<<argv[1]<<endl;
-    return 0;
-   }
+     Cronologia mi_cronologia, resultado;
+     f >> mi_cronologia;
 
-   Cronologia mi_cronologia;
-   f >> mi_cronologia;
-
-   int nyears = mi_cronologia.getNumeroFechas();
-   int neventos = mi_cronologia.getNumeroEventos();
-   int nmax = mi_cronologia.getMaximoEventos();
-   int promedio = neventos / nyears;
-
-   cout << "El número total de años es " << nyears << endl;
-   cout << "El número total de eventos guardados es " << neventos << endl;
-   cout << "El máximo de eventos por año es " << nmax << endl;
-   cout << "El promedio de eventos por año es " << promedio;
+     int inf = atoi(argv[2]);
+     int sup = atoi(argv[3]);
 
 
+     resultado = mi_cronologia.EventsBetween(inf,sup);
+
+     if(argc == 4) {
+     cout << "Los eventos dentro del intervalo son: " << endl;
+     cout << resultado << endl;
+   } else {
+     ofstream f2 (argv[4]);
+     if (!f2){
+      cout<<"No ha sido posible abrir el fichero "<<argv[4]<<endl;
+      return 0;
+    }
+    f2 << resultado;
+  }
 }
