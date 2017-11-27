f29296ebae66d601caf2e1a2e6292b5e66e56ba9
Víctor Castro Serrano <32845717+vicase@users.noreply.github.com>
2017-11-27 14:06:48 +0100

    Funciones documentadas

--- Práctica 4 - STL e Iteradores/include/cronologia.h
+++ Práctica 4 - STL e Iteradores/include/cronologia.h
@@ -24,10 +24,10 @@ typedef set<string>::const_iterator const_iterator_set;
   *  @brief T.D.A. Cronologia
   *
   * Una instancia @e c del tipo de datos abstracto @c Cronologia es un objeto
-  * con 1 campo, un vector dinámico de objetos de FechaHistorica
+  * con 1 campo, un map<int,FechaHistorica> con los años y los eventos históricos correspondientes
   *
   * @author  Maxi Suárez van Gelderen
-  * @author Victor Castro Serrano
+  * @author  Victor Castro Serrano
   * @date
   */
 
@@ -64,13 +64,21 @@ public:
     *@return Número de FechasHistoricas que tiene nuestra cronología
     */
   int getNumeroFechas() const;
+ 
+ /**
+    * @brief Nos devuelve el número total de eventos históricos almacenados en la cronología
+    * @return Número de eventos histricos que tiene la cronologa
+    */
 
   int getNumeroEventos() const;
+ 
+  /**
+    * @brief Nos devuelve el mayor número de eventos almacenados para un año
+    * @return Mayor número de eventos almacenados para un año
+    */
 
   int getMaximoEventos() const;
-
-
-
+ 
   /**
     *@brief Devuelve la FechaHistorica que contiene nuestro
     *vector cronologico en la posicion i
@@ -86,19 +94,17 @@ public:
   void addFecha(FechaHistorica f);
 
   /**
-    *@brief Sobreescribe una fecha ya existente y la ordena cronológicamente
-    *@param i posición donde se quiere insertar la fecha
-    *@param f FechaHistorica que se queire añadir
+    *@brief Borra la FechaHistorica correspondiente con el año del argumento
+    *@param y Año de los eventos a borrar
     */
 
   void deleteFecha(int y);
 
 
   /**
-    *@brief Busca un año en la cronología y devuelve la posicion del vector
-    *cron en la que se encuentra.
+    *@brief Devuelve si existe una FechaHistorica con el año dado como argumento
     *@param f Año que se quiere buscar
-    *@return Devuelve posicion del vector del año. Si no está, devuelve -1
+    *@return bool en verdadero si existe la FechaHistorica con el año dado
     */
   bool existYear(int f) const;
 
@@ -112,7 +118,7 @@ public:
   Cronologia EventsBetween(int y1, int y2);
 
   /**
-    *@brief Busca los acontecimientos que ocurrieron entre dos años
+    *@brief Busca los acontecimientos que contengan el string dado como argumento
     *@param s string que contiene el evento a buscar
     *@param matches Cronologia devuelta por referencia donde se almacenará la Cronologia de eventos buscados
     *@return Devuelve un bool indicando si estaba el evento en la cronología
@@ -140,6 +146,13 @@ public:
     *@param c Cronología donde queremos cargar el Flujo
     */
   friend istream& operator>> (istream& is, Cronologia& c);
+ 
+ /**
+    *@brief Dadas dos cronologas, las une y las almacena en la tercera dada como argumento por referencia
+    *@param c1 Primera cronología a unir
+    *@param c2 Segunda cronología a unir
+    *@param res Cronología que contendrá la unión de ambas, devuelta por referencia
+    */
 
   friend void UnirCronologias(const Cronologia& c1, const Cronologia& c2, Cronologia& res);
 
